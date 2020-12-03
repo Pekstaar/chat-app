da069b816a47cf9b57d267e08bf9aae6db4a797b
Cruiz <pekstaar@gmail.com>
2020-12-03 08:41:46 +0300

    redux-saga

--- src/components/chat-form/Chat-Form.js
+++ src/components/chat-form/Chat-Form.js
@@ -1,32 +1,68 @@
 import React, { useState } from 'react';
 import att from '../../images/icons/attachment-logo.svg'
+import FormButton from '../controls/FormButton';
 
 import './Chat-Form.css';
 
-function ChatForm({ onMessageSubmitted }) {
 
-    const [message, setMessage] = useState('')
+// const [message, setMessage] = useState('')
 
-    const handleChange = (e) => {
-        setMessage(e.target.value);
-    }
+// const handleChange = (e) => {
+//     setMessage(e.target.value);
+// }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        onMessageSubmitted(message);
-        setMessage('');
+// const handleSubmit = (e) => {
+//     e.preventDefault()
+//     onMessageSubmitted(message);
+//     setMessage('');
+// }
+
+const isMessageEmpty = (textMessage) => {
+    return adjustTextMessage(textMessage).length === 0;
+}
+
+const adjustTextMessage = (textMessage) => {
+    return textMessage.trim();
+};
+
+const ChatForm = ({ onMessageSubmitted, selectedConversation }) => {
+    const [textMessage, setTextMessage] = useState('');
+    const disableButton = isMessageEmpty(textMessage);
+    let formContents = null;
+    let handleFormSubmit = null;
+
+    if (selectedConversation) {
+        formContents = (
+            <>
+                <div title="Add Attachment">
+                    <img src={att} alt="Add Attachment" />
+                </div>
+                <input
+                    type="text"
+                    placeholder="type a message"
+                    value={textMessage}
+                    onChange={(e) => { setTextMessage(e.target.value); }}
+                />
+
+                <FormButton disabled={disableButton}>Send</FormButton>
+            </>
+        );
+
+        handleFormSubmit = (e) => {
+            e.preventDefault();
+
+            if (!isMessageEmpty(textMessage)) {
+                onMessageSubmitted(textMessage);
+                setTextMessage('');
+            }
+        };
     }
 
     return (
-        <form id="chat-form" onSubmit={handleSubmit}>
-            <img src={att} alt="Add Attachment" />
-            <input
-                type="text"
-                placeholder="type a message"
-                value={message}
-                onChange={handleChange}
-            />
-            <button type='submit'>Send</button>
+        <form id="chat-form" onSubmit={handleFormSubmit}>
+            {/* <img src={att} alt="Add Attachment" /> */}
+            {formContents}
+            {/* <button type='submit'>Send</button> */}
         </form>
     );
 }
